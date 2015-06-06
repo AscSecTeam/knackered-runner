e0ac9599f4ec60102191b9035721d83bff311538
Jeff Andolora <andolojm@alfredstate.edu>
2015-06-06 14:29:05 -0400

    Basic Auth implementation

--- Knackered-Web/routes.py
+++ Knackered-Web/routes.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+from flask.ext.login import LoginManager, UserMixin, login_required
+from User import User
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-@app.route('/')
-def home():
+@login_manager.request_loader
+def load_user(request):
+    token = request.headers.get('Authorization')
+
+    if token is not None:
+        user_entry = User.get(request.authorization.username)
+        if user_entry is not None:
+            user = User(user_entry[0], user_entry[1])
+            if user.password == request.authorization.password:
+                print user.password
+                return user
+    return None
+
+@app.route("/", methods=["GET"])
+def index():
     return render_template('index.html')
 
+@app.route('/configure', methods=["GET"])
+@login_required
+def admin_config():
+    return render_template('configure.html')
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.config["SECRET_KEY"] = "Does this even do anything?"
+    app.run(port=5000,debug=True)
